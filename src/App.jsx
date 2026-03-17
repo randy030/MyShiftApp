e5de002c013efdc0543dbff7b5d32de09e8731bd
Boss <randy22444289@gmail.com>
2026-03-17 16:58:42 +0800

    V7.8.2

--- src/App.jsx
+++ src/App.jsx
@@ -1939,5 +1939,3 @@ const SettingsView = ({ users, currentUserInfo, leaveTypes, shiftTypes, inventor
     </div>
   );
 };
-
-const CreditCard = ({ size }) => <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>;
